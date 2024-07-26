68af26e82cc2ee9e249f2c1536bff089af0d67a3
Andrey Sham <avshamatrin@gmail.com>
2024-07-26 17:27:44 +0300

    Add model and service to Ahgular. GET adn POST data between Backend end Frontend.

--- TeamChallenge_OnlineStore.Web/src/app/app.component.ts
+++ TeamChallenge_OnlineStore.Web/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Test } from '../models/test';
+import { TestService } from '../services/test.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,26 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  tests: Test[] = [];
   title = 'onlinestoreng';
+
+  constructor(private testService : TestService){}
+
+  ngOnInit(): void {
+    this.testService.getAll().subscribe(data =>{
+      this.tests = data;
+    })
+    //this.tests.push({ id: 1, testData: "TCh" });
+  }
+
+  onButtonClick() : void {
+    let test = new Test();
+    test.id = 0;
+    test.data = "22";
+
+    this.testService.add(test).subscribe(data => {
+      console.log("Data is saved");
+    })
+  }
 }
